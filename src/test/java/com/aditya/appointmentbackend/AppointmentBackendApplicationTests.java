967d75ef26eecb7afd5922a4644802a83ccc5a3a
Aditya <adityakr365@gmail.com>
2025-12-18 20:47:16 +0530

    adding basic project structure

---
+++ src/test/java/com/aditya/appointmentbackend/AppointmentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aditya.appointmentbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
